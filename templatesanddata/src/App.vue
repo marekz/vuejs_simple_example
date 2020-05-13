163d937d64196699feaa0f0e64ec4f9a3242728b
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-13 23:19:29 +0200

    Data binding examples - call methods from text interpolation binding'

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -2,8 +2,8 @@
   <div class="bg-primary text-white text-center m-2 p-2">
     <h3>This is a component</h3>
     <h4>Product: {{ productName }}</h4>
-    <h4>Price: {{ lowTotalPrice.toFixed(2) }} zł (niższa stawka)</h4>
-    <h4>Price: {{ highTotalPrice.toFixed(2) }} zł (wyższa stawka)</h4>
+    <h4>Price: {{ getTotalPrice(this.lowTaxRate).toFixed(2) }} zł (niższa stawka)</h4>
+    <h4>Price: {{ getTotalPrice(this.highTaxRate).toFixed(2) }} zł (wyższa stawka)</h4>
   </div>
 </template>
 
@@ -16,14 +16,6 @@ export default {
       price: 203,
       lowTaxRate: 19,
       highTaxRate: 23
-    }
-  },
-  computed: {
-    lowTotalPrice: function () {
-      return this.getTotalPrice(this.lowTaxRate);
-    },
-    highTotalPrice: function() {
-      return this.getTotalPrice(this.highTaxRate);
     }
   },
   methods: {
