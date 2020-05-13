8dcb32e1c35427aed9b0f1c97defa89033ddfd1b
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-13 22:06:57 +0200

    Data binding examples - simple example with computed properties

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-primary text-white text-center m-2 p-2">
-    <h3>To jest komponent</h3>
-    <h4>Name: {{ name }}, {{ age }} years</h4>
-    <p>{{ message }}</p>
+    <h3>This is a component</h3>
+    <h4>Product: {{ name }}</h4>
+    <h3>Price: {{ totalPrice.toFixed(2) }}</h3>
   </div>
 </template>
 
@@ -11,21 +11,15 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      name: "Kajtek",
-      age: 10,
-      message: "Witaj świecie!"
+      productName: "Bajtek",
+      price: 203,
+      taxRate: 23
+    }
+  },
+  computed: {
+    totalPrice: function() {
+      return this.price + (this.price * (this.taxRate / 100));
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
