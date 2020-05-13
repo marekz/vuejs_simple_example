4fa616aabafcfd85af5c9419aee697e1343d750e
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-13 22:55:29 +0200

    Data binding examples - reactivity and computed values

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="bg-primary text-white text-center m-2 p-2">
     <h3>This is a component</h3>
-    <h4>Product: {{ name }}</h4>
-    <h3>Price: {{ totalPrice.toFixed(2) }}</h3>
+    <h4>Product: {{ productName }}</h4>
+    <h4>Price: {{ totalPrice.toFixed(2) }}</h4>
   </div>
 </template>
 
 <script>
+  // import "core-js/modules/es.array.concat";
 export default {
   name: 'MyComponent',
   data: function () {
@@ -18,7 +19,9 @@ export default {
   },
   computed: {
     totalPrice: function() {
-      return this.price + (this.price * (this.taxRate / 100));
+      let tp = this.price + (this.price * (this.taxRate / 100));
+      console.log(`Obliczono: ${tp} (${this.taxRate})`);
+      return tp;
     }
   }
 }
