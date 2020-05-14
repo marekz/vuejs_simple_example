ef3a94e3fbe32b265dc96c42b132e9da7e8086c2
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-14 22:35:46 +0200

    Directive v-if - choosing elements to display with v-if, v-else-if and v-else

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container-fluid text-center">
     <div class="bg-primary text-white m-2 p-3">
-      <h3 v-if="showElements">Produkt: <span v-text="name"></span></h3>
-      <h3 v-else>Cena: <span v-text="price"></span></h3>
+      <h3 v-if="counter % 3 == 0">Produkt: <span v-text="name"></span></h3>
+      <h3 v-else-if="counter % 3 == 1">Cena: <span v-text="price"></span></h3>
+      <h3 v-else>Kategoria: <span v-text="category"></span></h3>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
       Wciśnij mnie
@@ -17,12 +18,14 @@ export default {
     return {
       name: "Kamizelka ratunkowa",
       price: 275,
-      showElements: true
+      showElements: true,
+      category: "Sporty wodne",
+      counter: 0
     }
   },
   methods: {
     handleClick() {
-      this.showElements = !this.showElements;
+      this.counter++;
     }
   }
 }
