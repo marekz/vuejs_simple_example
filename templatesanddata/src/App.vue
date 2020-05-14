eedd6066003b0727f780bbd2c4396a2c7fe17bd2
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-14 22:23:37 +0200

    Directive v-if - displaying neightbour elements - wrong way

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -2,6 +2,12 @@
   <div class="container-fluid text-center">
     <div class="bg-primary text-white m-2 p-3">
       <h3>Produkt: <span v-text="name"></span></h3>
+      <ul class="text-left">
+        <li>List element</li>
+        <li v-if="showElements">{{ name }}</li>
+        <li v-if="showElements">{{ price }}</li>
+        <li>Another list element...</li>
+      </ul>
       <h4 v-if="showElements">{{ price }}</h4>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
