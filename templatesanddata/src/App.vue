7e083fcd3f4f2415cc5a0854b13873f27785084a
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-14 22:11:27 +0200

    Directives - binding  text - v-text and v-html

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -2,6 +2,7 @@
   <div class="container-fluid text-center">
     <div class="bg-primary text-white m-2 p-3">
       <h3>Produkt: <span v-text="name"></span></h3>
+      <span v-html="fragment"></span>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
       Wciśnij mnie
@@ -14,7 +15,8 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      name: "Kamizelka ratunkowa"
+      name: "Kamizelka ratunkowa",
+      fragment: '<div class="form-group">\n                    Hasło\n                    <input class="form-control" />\n                  </div>'
     }
   },
   methods: {
