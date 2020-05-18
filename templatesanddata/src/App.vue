87991b7f34b83e47d6a69a4b316f4bd709218da3
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-18 09:34:28 +0200

    Directive bind - use object to CSS class configuration

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="container-fluid text-center">
     <div class="bg-primary text-white m-2 p-3">
-      <h3 v-bind:class="elemClasses">Produkt: <span v-text="name"></span></h3>
-      <!--
-        :class is short method v-bind
-        <h3 :class="elemClasses">Produkt: <span v-text="name"></span></h3>
-       -->
+      <h3 v-bind:class="elemClasses" class="display-4">Produkt: <span v-text="name"></span></h3>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
       Wciśnij mnie
+    </button>
+    <button v-on:click="handleOtherClick" class="btn btn-primary">
+      Lub mnie
     </button>
   </div>
 </template>
@@ -19,19 +18,25 @@ export default {
   data: function () {
     return {
       name: "Kamizelka ratunkowa",
-      highlight: false
+      highlight1: false,
+      highlight2: false
     }
   },
   computed: {
     elemClasses() {
-      return this.highlight
-      ? ["bg-light", "text-dark", "display-4"]
-              : ["bg-dark", "text-light", "p-2"]
+      return {
+        "text-dark": this.highlight1,
+        "bg-light": this.highlight2,
+      }
     }
   },
   methods: {
     handleClick() {
-      this.highlight = !this.highlight;
+      this.highlight1 = !this.highlight1;
+    },
+
+    handleOtherClick() {
+      this.highlight2 = !this.highlight2;
     }
   }
 }
