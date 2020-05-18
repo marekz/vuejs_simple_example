fd49c1bc1295f74adf6b432030b6ef056621d1db
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-18 12:10:25 +0200

    Directive bind - set your own attribut

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container-fluid text-center">
     <div class="bg-primary text-white m-2 p-3">
-      <h3 v-bind:class="elemClasses" class="display-4">Produkt: <span v-text="name"></span></h3>
+      <h3 v-bind:data-size="size" class="display-4">Produkt: <span v-text="name"></span></h3>
     </div>
     <button v-on:click="handleClick" class="btn btn-primary">
       Wciśnij mnie
-    </button>
-    <button v-on:click="handleOtherClick" class="btn btn-primary">
-      Lub mnie
     </button>
   </div>
 </template>
@@ -18,26 +15,22 @@ export default {
   data: function () {
     return {
       name: "Kamizelka ratunkowa",
-      highlight1: false,
-      highlight2: false
+      highlight: false
     }
   },
   computed: {
-    elemClasses() {
-      return {
-        "text-dark": this.highlight1,
-        "bg-light": this.highlight2,
-      }
+    size() {
+      return this.highlight ? "big": "small";
     }
   },
   methods: {
     handleClick() {
-      this.highlight1 = !this.highlight1;
-    },
-
-    handleOtherClick() {
-      this.highlight2 = !this.highlight2;
+      this.highlight = !this.highlight;
     }
   }
 }
 </script>
+<style>
+  [data-size='big'] { font-size: 40pt !important; }
+  [data-size='small'] { font-size: 20pt !important; }
+</style>
