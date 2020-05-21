0cdecbc86cf7115019204c9d4aa702834fdec401
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-21 21:31:58 +0200

    Loop directive - iteration with alias

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="container-fluid">
-    <div class="bg-primary text-white m-2 p-3 text-center">
-      <h3>Produkt: {{ name }}</h3>
-    </div>
+    <h2 class="bg-primary text-white text-center p-3">Produkt</h2>
+    <table class="table table-sm table-border table-striped text-left">
+      <tr><th>Nazwa</th><th>Cena</th></tr>
+      <tbody>
+      <tr v-for="p in products" :key = "p">
+        <td>{{ p.name }}</td>
+        <td>{{ p.price }}</td>
+      </tr>
+      </tbody>
+    </table>
     <div class="text-center">
       <button v-on:click="handleClick" class="btn btn-primary">
         Wciśnij mnie
@@ -16,7 +23,10 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      name: "Kamizelka ratunkowa"
+      products: [
+        { name: "Kajak", price: 275},
+        { name: "Kamizelka ratunkowa", price: 48.95},
+        { name: "Piłka nożna", price: 19.50}]
     }
   },
   methods: {
