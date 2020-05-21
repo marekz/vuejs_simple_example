3310a85c4d4c89c250ec7266f8fe4ce27cce4660
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-21 18:18:12 +0200

    Loop directive - preparation to lesson

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="container-fluid text-center">
-    <div class="bg-primary text-white m-2 p-3">
-      <h3 v-bind:text-content.prop="textContent"></h3>
+  <div class="container-fluid">
+    <div class="bg-primary text-white m-2 p-3 text-center">
+      <h3>Produkt: {{ name }}</h3>
     </div>
-    <button v-on:click="handleClick" class="btn btn-primary">
-      Wciśnij mnie
-    </button>
+    <div class="text-center">
+      <button v-on:click="handleClick" class="btn btn-primary">
+        Wciśnij mnie
+      </button>
+    </div>
   </div>
 </template>
 
@@ -14,13 +16,7 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-      name: "Kamizelka ratunkowa",
-      highlight: false
-    }
-  },
-  computed: {
-    textContent() {
-      return this.highlight ? "Podświetlenie" : `Produkt: ${this.name}`;
+      name: "Kamizelka ratunkowa"
     }
   },
   methods: {
