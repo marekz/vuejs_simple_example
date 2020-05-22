668424322cac3f98d0180e343e3d699dea172970
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-22 10:15:49 +0200

    Loop directive - define and use index

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -2,9 +2,11 @@
   <div class="container-fluid">
     <h2 class="bg-primary text-white text-center p-3">Produkt</h2>
     <table class="table table-sm table-border table-striped text-left">
-      <tr><th>Nazwa</th><th>Cena</th></tr>
+      <tr><th>Indeks</th><th>Nazwa</th><th>Cena</th></tr>
       <tbody>
-      <tr v-for="p in products" :key = "p.name">
+      <tr v-for="(p, i) in products"
+          v-bind:key="p.name" v-bind:odd="i % 2 == 0">
+        <td>{{ i + 1 }}</td>
         <td>{{ p.name }}</td>
         <td>{{ p.price | currency }}</td>
       </tr>
@@ -26,7 +28,10 @@ export default {
       products: [
         { name: "Kajak", price: 275},
         { name: "Kamizelka ratunkowa", price: 48.95},
-        { name: "Piłka nożna", price: 19.50}]
+        { name: "Piłka nożna", price: 19.50},
+        { name: "Chorągiewki narożne", price: 39.95},
+        { name: "Stadion", price: 79500},
+        { name: "Myśląca czapeczka", price: 16}]
     }
   },
   filters: {
@@ -43,6 +48,9 @@ export default {
 }
 </script>
 <style>
+  [odd] {
+    background-color: lightblue;
+  }
   #tagged {
     background-color: coral;
   }
