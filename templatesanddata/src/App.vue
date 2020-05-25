944c336beb493bbbf8e143761182749ff8b62d22
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-25 13:33:38 +0200

    Event propagation - Self modiffier

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click.capture="handleClick">
       Element zewnętrzny
-      <div id="middle-element" class="bg-secondary p-4" v-on:click.capture="handleClick">
+      <div id="middle-element" class="bg-secondary p-4" v-on:click.self="handleClick">
         Element pośredni
         <div id="inner-element" class="bg-info  p-4"
              v-on:click="handleClick">
