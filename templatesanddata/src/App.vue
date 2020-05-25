9fcd62a23e2c83a22d0e36a3a95cfbb3cb4854fc
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-25 11:37:47 +0200

    Event handler - basic events with method execution

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="container-fluid">
-    <div class="text-center p-3 m-2">
-      <h3 class="btn btn-primary m-2" v-on:click="name = $event.type">{{ name }}</h3>
-      <h3 class="btn btn-secondary m-2" v-on:mousedown="name = $event.type">{{ name }}</h3>
-      <h3 class="btn btn-dark m-2" v-on:mousemove="name = $event.type">{{ name }}</h3>
-      <h3 class="btn btn-success m-2" v-on:keydown="name = $event.type">{{ name }}</h3>
+    <div class="bg-primary text-white text-center p-3 m-2">
+      <h3 v-on:click="handleEvent">{{ name }}</h3>
     </div>
   </div>
 </template>
@@ -15,6 +12,11 @@
   data: function () {
     return {
         name: "Kamizelka ratunkowa"
+    }
+  },
+  methods: {
+    handleEvent($event) {
+      this.name = $event.type;
     }
   }
 }
