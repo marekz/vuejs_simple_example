c80d409fd6cc87238f0c54ccb36b5e87e4d3fcc5
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-25 13:08:47 +0200

    Event propagation example

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,51 +1,25 @@
 <template>
   <div class="container-fluid">
-    <h3 class="bg-primary text-white text-center mt-2 p-2">{{ message }}</h3>
-    <table class="table table-sm table-striped table-bordered">
-      <tr><th>Index</th><th>Nazwa</th><th>Akcje</th></tr>
-      <tr v-for="(name, index) in names" v-bind:key="name">
-        <td>{{index}}</td>
-        <td>{{name}}</td>
-        <td>
-          <button class="btn btn-sm bg-primary text-white"
-                  v-on="buttonEvents"
-                  v-bind:data-name="name">
-            Wybierz
-          </button>
-        </td>
-      </tr>
-    </table>
+    <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click="handleClick">
+      Element zewnętrzny
+      <div id="middle-element" class="bg-secondary p-4" v-on:click="handleClick">
+        Element pośredni
+        <div id="inner-element" class="bg-info  p-4"
+             v-on:click="handleClick">
+          Element wewnętrzny
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
   name: 'MyComponent',
-  data: function () {
-    return {
-      buttonEvents: {
-        click: this.handleClick,
-        mousemove: this.handleMouseEvent,
-        mouseleave: this.handleMouseEvent
-      },
-      counter: 0,
-      message: "Gotowy",
-      names: ["Kamizelka ratunkowa", "Kajak", "Piłka nożna", "Stadion"]
-    }
-  },
   methods: {
     handleClick($event) {
-      let name = $event.target.dataset.name;
-      this.message = `Select: ${name}`;
-    },
-    handleMouseEvent($event) {
-      let name = $event.target.dataset.name;
-      if ($event.type == "mousemove") {
-        this.message = `Ruch w ${name} ${this.counter++}`
-      } else {
-        this.counter = 0;
-        this.message = "Gotowy";
-      }
+      console.log(`handleClick target: ${$event.target.id}`
+      + ` currentTarget: ${$event.currentTarget.id}`);
     }
   }
 }
