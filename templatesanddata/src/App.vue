bd4fd3870d7529c89816ebd66ac764b0108b015e
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-25 13:23:34 +0200

    Event propagation - Capture modiffier

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-fluid">
-    <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click="handleClick">
+    <div id="outer-element" class="bg-primary p-4 text-white h3" v-on:click.capture="handleClick">
       Element zewnętrzny
-      <div id="middle-element" class="bg-secondary p-4" v-on:click="handleClick">
+      <div id="middle-element" class="bg-secondary p-4" v-on:click.capture="handleClick">
         Element pośredni
         <div id="inner-element" class="bg-info  p-4"
              v-on:click="handleClick">
