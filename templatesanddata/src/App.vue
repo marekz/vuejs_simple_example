785095aed4314865172f3170272d3352b35ff081
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-27 06:58:23 +0200

    VUEjs forms - binding select elements

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -4,9 +4,12 @@
       <div>Imię: {{ name }}</div>
     </div>
     <div class="bg-primary m-2 p-2 text-white">
-      <div class="form-check" v-for="n in allNames" v-bind:key="n">
-        <input class="form-check-input" type="radio" v-model="name" v-bind:value="n" />
-        <label class="form-check-label">{{ n }}</label>
+      <div class="form-group">
+        <label>Wybrane imiona: </label>
+        <select class="form-controll" v-model="name">
+          <option value="all">Wszystkie</option>
+          <option v-for="n in allNames" v-bind:key="n" v-bind:value="n">Tylko {{ n}}</option>
+        </select>
       </div>
     </div>
   </div>
