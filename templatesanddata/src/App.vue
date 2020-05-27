a52319d25e7e07e6caa6b78500139fb4ee97a3a4
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-05-27 06:48:19 +0200

    VUEjs forms - simple bidirectional binding choice options

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -2,20 +2,11 @@
   <div class="container-fluid">
     <div class="bg-info m-2 p-2 text-white">
       <div>Imię: {{ name }}</div>
-      <div>Czy ma dostęp administracyjny: {{ hasAdminAccess }}</div>
     </div>
     <div class="bg-primary m-2 p-2 text-white">
-      <div class="form-check">
-        <input class="form-check-input" type="radio" v-model="name" value="Janek" />
-        <label class="form-check-label">Janek</label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" v-model="name" value="Alicja" />
-        <label class="form-check-label">Alicja</label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" v-model="hasAdminAccess" />
-        <label class="form-check-label">Czy ma dostęp administracyjny?</label>
+      <div class="form-check" v-for="n in allNames" v-bind:key="n">
+        <input class="form-check-input" type="radio" v-model="name" v-bind:value="n" />
+        <label class="form-check-label">{{ n }}</label>
       </div>
     </div>
   </div>
@@ -27,7 +18,7 @@
     data: function() {
       return {
         name: "Janek",
-        hasAdminAccess: true
+        allNames: ["Janek", "Alicja", "Magda"]
       }
     }
 }
