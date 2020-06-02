049269aec2b6a2fe1734e847f970916c839b006e
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-06-02 06:49:31 +0200

    VUEjs forms - bidirectional binding - array

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="container-fluid">
-    <div class="bg-info m-2 p-2 text-white">
-      <div>Imię: ** {{ name }} **</div>
+    <div class="container-fluid">
+        <div class="bg-info m-2 p-2 text-white">
+            <div>Wybrane miasta: {{ cities }}</div>
+        </div>
+        <div class="form-check m-2" v-for="city in cityNames" v-bind:key="city">
+            <label class="form-check-label">
+                <input type="checkbox" class="form-check-input" v-model="cities" v-bind:value="city"/> {{city}}
+            </label>
+        </div>
+        <div class="text-center">
+          <button v-on:click="reset" class="btn btn-info">Reset</button>
+        </div>
     </div>
-    <div class="form-group">
-        <label>Liczba: </label>
-        <input type="text" class="form-control" v-model.trim="name" />
-      </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    name: 'MyComponent',
-    data: function() {
-      return {
-        name: "Janek"
-      }
+    export default {
+        name: 'MyComponent',
+        data: function () {
+            return {
+                cityNames: ["Warszawa", "Zamość", "Szczecin", "Gdańsk", "Kraków", "Poznań", "Lublin"],
+                cities: []
+            }
+        },
+        methods: {
+            reset() {
+                this.cities = [];
+            }
+        }
     }
-}
 </script>
