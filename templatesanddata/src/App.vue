60da9a2f2e2dfa58fb4917b91f59f48b51e1c848
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-06-06 09:14:38 +0200

    VUEjs forms - bidirectional binding - use your own values in check, select and radio

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -5,7 +5,29 @@
         </div>
         <div class="form-check m-2">
             <label class="form-check-label">
-              <input type="checkbox" class="form-check-input" v-model="dataValue" true-value="bg-primary" false-value="bg-info"/> Ciemny kolor
+              <input type="checkbox" class="form-check-input"
+                v-model="dataValue" v-bind:true-value="darkColor" v-bind:false-value="lightColor"/> Ciemny kolor
+            </label>
+        </div>
+        <div class="form-group m-2 p-2 bg-secondary">
+            <label class="form-check-label">Kolor</label>
+            <select v-model="dataValue" class="form-control">
+                <option v-bind:value="darkColor">Ciemny kolor</option>
+                <option v-bind:value="lightColor">Jasny kolor</option>
+            </select>
+        </div>
+        <div class="form-check-inline m-2">
+            <label class="form-check-label">
+                <input type="radio" class="form-check-input"
+                    v-model="dataValue" v-bind:value="lightColor" />
+                    Jasny kolor
+            </label>
+        </div>
+        <div class="form-check-inline m-2">
+            <label class="form-check-label">
+                <input type="radio" class="form-check-input"
+                    v-model="dataValue" v-bind:value="darkColor" />
+                    Ciemny kolor
             </label>
         </div>
     </div>
@@ -16,7 +38,9 @@
         name: 'MyComponent',
         data: function () {
             return {
-                dataValue: "bg-info",
+                darkColor: "bg-primary",
+                lightColor: "bg-info",
+                dataValue: "bg-info"
             }
         }
     }
