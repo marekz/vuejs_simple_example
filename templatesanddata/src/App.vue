eab2b374ef959e442095b38f3b5b1911932b95ee
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-06-06 10:10:23 +0200

    VUEjs forms - form validation, rule definition, validation functions

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container-fluid">
-        <div class="btn-primary text-white my-2 p-2">
-            Nazwa: {{ name }}, Kategoria: {{ category }}, cena: {{ price }}
+        <div class="bg-danger text-white my-2 p-2" v-if="errors">
+            <h5>Znaleziono następujące problemy:</h5>
+            <ul>
+                <template v-for="(errors) in validationErrors">
+                    <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+                </template>
+            </ul>
         </div>
         <form v-on:submit.prevent="handleSubmit">
             <div class="form-group">
@@ -24,18 +29,48 @@
 </template>
 
 <script>
+    import validation from "./validationRules";
+    import Vue from "vue";
+
     export default {
         name: 'MyComponent',
         data: function () {
             return {
                 name: "",
                 category: "",
-                price: 0
+                price: 0,
+                validationErrors: {},
+            }
+        },
+        computed: {
+            errors() {
+                return Object.values(this.validationErrors).length > 0;
             }
         },
         methods: {
+            validate(propertyName, value) {
+                let errors = [];
+                Object(validation)[propertyName].forEach(v => {
+                    if (!v.validator(value)) {
+                        errors.push(v.message);
+                    }
+                });
+                if (errors.length > 0) {
+                    Vue.set(this.validationErrors, propertyName, errors);
+                } else {
+                    Vue.delete(this.validationErrors, propertyName);
+                }
+            },
+            validateAll() {
+                this.validate("name", this.name);
+                this.validate("category", this.category);
+                this.validate("price", this.price);
+                return this.errors;
+            },
             handleSubmit() {
-                console.log(`Wysłano formularz: ${this.name} ${this.category}` + `${this.price}`);
+                if (this.validateAll()) {
+                    console.log(`WYSŁANO FORMULARZ: ${this.name} ${this.category}` + ` ${this.price}`);
+                }
             }
         }
     }
