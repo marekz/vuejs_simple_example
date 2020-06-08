b72d0acb211ac5a2ebfe3d975280b48f61a21cb4
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-06-08 20:50:27 +0200

    VUEjs - Component - date binding

--- templatesanddata/src/components/Child.vue
+++ templatesanddata/src/components/Child.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="bg-primary text-white text-center m-2 p-3 h6">
-        Komponent-dziecko
+        {{ message }}
+        <div class="form-group m-1">
+            <input v-model="message" class="form-control" />
+        </div>
     </div>
 </template>
+<script>
+    export default {
+        data: function() {
+            return {
+                message: "To jest komponent-dziecko"
+            }
+        }
+    }
+</script>
