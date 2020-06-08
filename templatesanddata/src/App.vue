64161a34106835374fa0629e0e56bd5f497fc5b6
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-06-08 20:57:35 +0200

    VUEjs - Components isolation

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="bg-secondary text-white text-center my-2 p-2 h5">
-        Komponent-rodzic
+    <div class="bg-secondary text-white text-center m-2 p-2 h5">
+        {{ message }}
+        <MyFeature></MyFeature>
         <child-component></child-component>
     </div>
 </template>
 
 <script>
+    import ChildComponent from "./components/Child";
+
     export default {
-        name: 'App'
+        name: 'App',
+        components: {
+            MyFeature: ChildComponent
+        },
+        data: function() {
+            return {
+                message: "To jest komponent-rodzic"
+            }
+        }
     }
 </script>
