ad93f6b7b34ebd99ad12ed8eaed4c11b2d10d0df
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-06-08 20:29:52 +0200

    VUEjs - Component - basic usage

--- templatesanddata/src/App.vue
+++ templatesanddata/src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="bg-secondary text-white text-center my-2 p-2 h5">
-        Komponent główny
+        Komponent-rodzic
+        <ChildComponent></ChildComponent>
     </div>
 </template>
 
 <script>
+    import ChildComponent from "./components/Child"
     export default {
         name: 'App',
+        components: {
+            ChildComponent
+        }
     }
 </script>
---
+++ templatesanddata/src/components/Child.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-primary text-white text-center m-2 p-3 h6">
+        Komponent-dziecko
+    </div>
+</template>
